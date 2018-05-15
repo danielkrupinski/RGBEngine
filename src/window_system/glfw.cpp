d260cb41a7c7f00f1eda11c6089c788c9bcf0a10
Daniel Krupiński <danielkrupinski@outlook.com>
2018-05-15 18:51:53 +0200

    Add int rgbCreateWindow(int width, int height) definition

--- src/window_system/glfw.cpp
+++ src/window_system/glfw.cpp
@@ -3,5 +3,18 @@
 
 int rgbCreateWindow(int width, int height)
 {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+    GLFWwindow* window = glfwCreateWindow(800, 600, "GLFW Window", NULL, NULL);
+
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeCurentContext(window);
+    return 0;
 }
