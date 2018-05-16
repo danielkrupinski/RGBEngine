56d1e743bdf67fb18f5cfceb58bf39c93e6a2460
Daniel Krupiński <danielkrupinski@outlook.com>
2018-05-16 18:49:29 +0200

    Update void graphicsCreateWindow(void) definition

--- src/graphics/window.c
+++ src/graphics/window.c
@@ -1,2 +1,21 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <stdio.h>
+
+void graphicsCreateWindow(void)
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL 4.6 Window", NULL, NULL);
+
+    if (!window) {
+        fprintf(stderr, "Failed to create GLFW window!");
+        glfwTerminate();
+        exit(1);
+    }
+
+    glfwMakeCurrentContext(window);
+}
