a40c2c4270fefd78ca6ddc8989c716a20d0086b7
Daniel Krupiński <danielkrupinski@outlook.com>
2018-05-16 18:58:56 +0200

    Update void graphicsInitializeGlad(void) definition

--- src/graphics/glad.c
+++ src/graphics/glad.c
@@ -1,1 +1,9 @@
+#include <glad/glad.h>
 #include "graphics/glad.h"
+#include <stdio.h>
+
+void graphicsInitializeGlad(void)
+{
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+        fprintf(stderr, "Failed to initialize GLAD!\n");
+}
