c6c586747d35a363a53ad94e364615ebeb9e9082
Daniel Krupiński <danielkrupinski@outlook.com>
2018-05-18 18:03:12 +0200

    Enable window resizing

--- src/graphics/window.c
+++ src/graphics/window.c
@@ -20,6 +20,7 @@ void graphicsCreateWindow(void)
     }
 
     glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, graphicsResizeWindow); 
 }
 
 void graphicsResizeWindow(GLFWwindow* window, int width, int height)
