b970cb5a8394c59b9bd7596e5a17ceabec257b69
Daniel Krupiński <danielkrupinski@outlook.com>
2018-05-24 15:45:53 +0200

    Merge window.c and window.h in graphics.c and graphics.h

--- include/gfx/graphics.h
+++ include/gfx/graphics.h
@@ -4,6 +4,13 @@
 #include "shader.h"
 #include "window.h"
 
+#include <GLFW/glfw3.h>
+
+GLFWwindow* window;
+
+void graphicsCreateWindow(void);
+void graphicsResizeWindow(GLFWwindow*, int, int);
+
 void initGraphics(void);
 
 void renderGraphics(double);
--- src/gfx/graphics.c
+++ src/gfx/graphics.c
@@ -44,3 +44,27 @@ void graphicsInitializeGlad(void)
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
         fprintf(stderr, "Failed to initialize GLAD!\n");
 }
+
+void graphicsCreateWindow(void)
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    window = glfwCreateWindow(800, 600, "RGBEngine", NULL, NULL);
+
+    if (!window) {
+        fprintf(stderr, "Failed to create GLFW window!\n");
+        glfwTerminate();
+        exit(1);
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, graphicsResizeWindow);
+}
+
+void graphicsResizeWindow(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
