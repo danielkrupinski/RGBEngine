ef87df4db539e5827c030a027084ef8884a3f79f
Daniel Krupiński <danielkrupinski@outlook.com>
2018-05-24 17:12:36 +0200

    Remove files

--- include/engine.h
+++
@@ -1,9 +0,0 @@
-#ifndef RGBE_ENGINE_H
-#define RGBE_ENGINE_H
-
-#include "gfx/graphics.h"
-
-void rgbeInit(void);
-void rgbeRun(void);
-
-#endif // RGBE_ENGINE_H
--- include/gfx/shader.h
+++
@@ -1,9 +0,0 @@
-#ifndef RGBE_GRAPHICS_SHADER_H
-#define RGBE_GRAPHICS_SHADER_H
-
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-
-GLuint graphicsCompileShaders(void);
-
-#endif // RGBE_GRAPHICS_SHADER_H
