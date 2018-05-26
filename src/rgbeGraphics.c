c29e3ed74f9b0f2a6bd6475c6e09173e217b9c32
Daniel Krupiński <danielkrupinski@outlook.com>
2018-05-26 20:24:42 +0200

    Enable element buffer

--- src/rgbeGraphics.c
+++ src/rgbeGraphics.c
@@ -10,6 +10,7 @@ GLFWwindow* window;
 
 static unsigned int vbo;
 static unsigned int vao;
+static unsigned int ebo;
 static unsigned int shaderProgram;
 
 void rgbeInitGraphics(void)
@@ -52,7 +53,7 @@ void rgbeRenderGraphics(void)
         glClear(GL_COLOR_BUFFER_BIT);
         glUseProgram(shaderProgram);
         glBindVertexArray(vao);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
@@ -64,6 +65,7 @@ void rgbeShutdownGraphics(void)
 {
     glDeleteVertexArrays(1, &vao);
     glDeleteBuffers(1, &vbo);
+    glDeleteBuffers(1, &ebo);
     glfwTerminate();
 }
 
@@ -132,10 +134,14 @@ static void rgbeCompileShader(void)
 
     glGenVertexArrays(1, &vao);
     glGenBuffers(1, &vbo);
+    glGenBuffers(1, &ebo);
     glBindVertexArray(vao);
 
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
