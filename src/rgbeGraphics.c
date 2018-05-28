450dcc1e143b5ccfeeac47706fc3a71eab50b59a
Daniel Krupiński <danielkrupinski@outlook.com>
2018-05-28 20:22:03 +0200

    Update shader

--- src/rgbeGraphics.c
+++ src/rgbeGraphics.c
@@ -75,7 +75,7 @@ static void rgbeCompileShader(void)
                                      "layout (location = 0) in vec3 aPos;\n"
                                      "void main(void)\n"
                                      "{\n"
-                                     "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+                                     "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0f);\n"
                                      "}\0";
 
     const char* fragmentShaderSource = "#version 460 core\n"
