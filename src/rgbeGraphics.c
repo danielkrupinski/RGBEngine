dcc46d588a3f4558c13ebbdb4e9a1f2b8201ad1b
Daniel Krupiński <danielkrupinski@outlook.com>
2018-05-29 20:45:06 +0200

    Update vertex shader

--- src/rgbeGraphics.c
+++ src/rgbeGraphics.c
@@ -77,9 +77,12 @@ static void rgbeCompileShader(void)
 {
     const char* vertexShaderSource = "#version 460 core\n"
                                      "layout (location = 0) in vec3 aPos;\n"
+                                     "layout (location = 1) in vec3 aColor;\n"
+                                     "out vec3 ourColor;\n"
                                      "void main(void)\n"
                                      "{\n"
                                      "   gl_Position = vec4(aPos, 1.0f);\n"
+                                     "   ourColor = aColor;\n"
                                      "}\0";
 
     const char* fragmentShaderSource = "#version 460 core\n"
