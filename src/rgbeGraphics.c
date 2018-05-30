574e488f376341075e8d092db47dd05115144861
Daniel Krupiński <danielkrupinski@outlook.com>
2018-05-30 18:03:49 +0200

    Remove element buffer object

--- src/rgbeGraphics.c
+++ src/rgbeGraphics.c
@@ -11,7 +11,6 @@ GLFWwindow* window;
 
 static unsigned int vbo;
 static unsigned int vao;
-static unsigned int ebo;
 static unsigned int shaderProgram;
 
 void rgbeInitGraphics(void)
@@ -128,29 +127,17 @@ static void rgbeCompileShader(void)
         -0.5f,  0.5f, 0.0f,  1.0f, 0.0f, 0.0f
     };
 
-    unsigned int indices[] = {
-        0, 2, 6,
-        2, 4, 6
-    };
-
     glGenVertexArrays(1, &vao);
     glGenBuffers(1, &vbo);
-    glGenBuffers(1, &ebo);
     glBindVertexArray(vao);
 
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
-   glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
-
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
 }
 
 static void rgbeResizeWindow(GLFWwindow* window, int width, int height)
