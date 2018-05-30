d1cc83cdd8928a0a1122fda44bdf82ddb4fab89a
Daniel Krupiński <danielkrupinski@outlook.com>
2018-05-30 15:22:31 +0200

    Add colors to vertices array

--- src/rgbeGraphics.c
+++ src/rgbeGraphics.c
@@ -125,10 +125,10 @@ static void rgbeCompileShader(void)
     glDeleteShader(fragmentShader);
 
     float vertices[] = {
-         0.5f,  0.5f, 0.0f,
-         0.5f, -0.5f, 0.0f,
-        -0.5f, -0.5f, 0.0f,
-        -0.5f,  0.5f, 0.0f
+         0.5f,  0.5f, 0.0f,  1.0f, 0.0f, 0.0f,
+         0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,
+        -0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f,
+        -0.5f,  0.5f, 0.0f,  1.0f, 0.0f, 0.0f
     };
 
     unsigned int indices[] = {
