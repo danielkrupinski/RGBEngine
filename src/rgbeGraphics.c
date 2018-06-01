b1acc510d510949fab8a3a5bb071b8d8fba491ff
Daniel Krupiński <danielkrupinski@outlook.com>
2018-06-01 13:53:08 +0200

    Add element buffer object

--- src/rgbeGraphics.c
+++ src/rgbeGraphics.c
@@ -12,6 +12,7 @@ GLFWwindow* window;
 
 static unsigned int vbo;
 static unsigned int vao;
+static unsigned int ebo;
 static unsigned int shaderProgram;
 static const char* vertexShaderSource;
 static const char* fragmentShaderSource;
@@ -118,10 +119,14 @@ static void rgbeCompileShader(void)
 
     glGenVertexArrays(1, &vao);
     glGenBuffers(1, &vbo);
+    glGenBuffers(1, &ebo);
     glBindVertexArray(vao);
 
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
