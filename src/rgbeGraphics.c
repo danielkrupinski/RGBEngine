d261bde063c003998814db37c07bf0f2d1a8571c
Daniel Krupiński <danielkrupinski@outlook.com>
2018-06-01 18:49:22 +0200

    Do not include math.h

--- src/rgbeGraphics.c
+++ src/rgbeGraphics.c
@@ -1,5 +1,4 @@
 #include <glad/glad.h>
-#include <math.h>
 #include <rgbeGraphics.h>
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
@@ -153,7 +152,7 @@ static void rgbeCompileShader(void)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
     stbi_set_flip_vertically_on_load(true);
-    
+
     int width, height, nrChannels;
     unsigned char* data = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
 
