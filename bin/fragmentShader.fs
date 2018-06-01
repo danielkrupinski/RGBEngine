14c77a4c6bb7fb5fbb46606a4e45804f6b9acf67
Daniel Krupiński <danielkrupinski@outlook.com>
2018-06-01 14:53:02 +0200

    Update fragment shader to render both textures

--- bin/fragmentShader.fs
+++ bin/fragmentShader.fs
@@ -6,8 +6,9 @@ in vec3 ourColor;
 in vec2 TexCoord;
 
 uniform sampler2D texture1;
+uniform sampler2D texture2;
 
 void main(void)
 {
-    FragColor = texture(texture1, TexCoord) * vec4(ourColor, 1.0);
+    FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
 }
